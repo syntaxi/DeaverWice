70b75e1536cb79143bf996a0a1d7a25f0c0a707d
FriskyFoxx <42343594+FriskyFoxx@users.noreply.github.com>
2018-08-14 20:57:33 +1000

    PANSEXUAL
    commit ur mum

--- files/memes.js
+++ files/memes.js
@@ -1,10 +1,12 @@
+/**Memes and like, whatever */
 exports.registerBot = (bot) => {
     bot.on("message", msg => {
         if (msg.content.toLowerCase() === 'friskyfoxx') {
             msg.reply('Don\'t you mean\.\.\. Mother?');
         } else if (msg.content.toLowerCase().includes('unhook')) {
             msg.reply('JAR OF LIPS');
-            msg.reply('JAR OF DIPS');
+            msg.reply('JAR OF LIPS');
+            msg.reply('JAR OF LIPS');
         } else if (msg.content.toLowerCase().includes('nice')) {
             msg.reply('Join');
         } else if (msg.content.toLowerCase() === 'wd>help') {
@@ -13,8 +15,19 @@ exports.registerBot = (bot) => {
             let options = ["Male", "Female", "Trap"];
             let choice = Math.floor(Math.random() * options.length);
             msg.reply(options[choice])
+        } else if (msg.content.toLowerCase() === 'wd>sex') {
+                if (Math.random() < 0.4) {
+                    let options = ["Straight", "Gay", "Bi", "A", "*Clang! Clang!* you are Pansexual!", "Demi", "Poly", "Furry", "THE BIG GAY"];
+                    let choice = Math.floor(Math.random() * options.length);
+                    msg.reply(options[choice]);
+                } else {
+                    let options = ["Straight", "Trisexual"];
+                    let choice = Math.floor(Math.random() * options.length);
+                    msg.reply(options[choice]);
+                }                
         } else if (msg.content === 'what is my avatar') {
             msg.reply(message.author.avatarURL);
         }
     });
 };
+

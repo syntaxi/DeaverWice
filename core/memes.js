d3af7d2df795f87bbd71219842e2729f3654c7b5
FriskyFoxx <42343594+FriskyFoxx@users.noreply.github.com>
2018-08-16 21:23:53 +1000

    <.<

--- core/memes.js
+++ core/memes.js
@@ -24,9 +24,9 @@ class commands extends MessageHandler {
             " Now, I will stop them. Now I am changed, reborn through the energy of the Janus Key. Forever bound to the Void." +
             " Let it be known, if the Tenno want true salvation, they will lay down their arms, and wait for the baptism of my Janus key. It is time." +
             " I will teach these trespassers the redemptive power of my Janus key. They will learn it's simple truth. The Tenno are lost, and they will resist." +
-            " But I, Vor, will cleanse this place of their impurity.")
+            " But I, Vor, will cleanse this place of their impurity.");
         
-        this.registerIncludes("to pay respects", "F")
+        this.registerIncludes("to pay respects", "F");
         this.registerIncludes("captain vor", "Look at them, they come to this place when they know they are not pure. Tenno use the keys, but they are mere trespassers." +
             " Only I, Vor, know the true power of the Void. I was cut in half, destroyed, but through it's Janus Key, the Void called to me." +
             " It brought me here and here I was reborn. We cannot blame these creatures, they are being led by a false prophet, an impostor who knows not the secrets of the Void." +
@@ -34,7 +34,7 @@ class commands extends MessageHandler {
             " Now, I will stop them. Now I am changed, reborn through the energy of the Janus Key. Forever bound to the Void." +
             " Let it be known, if the Tenno want true salvation, they will lay down their arms, and wait for the baptism of my Janus key. It is time." +
             " I will teach these trespassers the redemptive power of my Janus key. They will learn it's simple truth. The Tenno are lost, and they will resist." +
-            " But I, Vor, will cleanse this place of their impurity.")
+            " But I, Vor, will cleanse this place of their impurity.");
         this.registerIncludes("did you really believe it would be this easy", "https://www.youtube.com/watch?v=wOygJJ7Zudk")
         this.registerIncludes("unhook", "JAR OF LIPS\nJAR OF LIPS\nJAR OF LIPS");
         this.registerIncludes("sonya", "ello cute boy! My name is Sonya I'm a very beautiful" +

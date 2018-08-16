433e52bbc986b0fc03c6c432f31adc43ca11f4a8
FriskyFoxx <42343594+FriskyFoxx@users.noreply.github.com>
2018-08-16 21:21:36 +1000

    vor

--- core/memes.js
+++ core/memes.js
@@ -17,7 +17,7 @@ class commands extends MessageHandler {
         this.registerEquals("wd>gender", commands.chooseGender);
         this.registerEquals("wd>sex", commands.chooseSex);
         this.registerEquals("nice", "join");
-        this.registerEquals("Look at them,", "/tts they come to this place when they know they are not pure. Tenno use the keys, but they are mere trespassers." +
+        this.registerEquals("Look at them,", "they come to this place when they know they are not pure. Tenno use the keys, but they are mere trespassers." +
             " Only I, Vor, know the true power of the Void. I was cut in half, destroyed, but through it's Janus Key, the Void called to me." +
             " It brought me here and here I was reborn. We cannot blame these creatures, they are being led by a false prophet, an impostor who knows not the secrets of the Void." +
             " Behold the Tenno, come to scavenge and desecrate this sacred realm. My brothers, did I not tell of this day? Did I not prophesize this moment?" +
